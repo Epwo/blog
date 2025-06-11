7e56536e1dd88e4bbbb1defe64453a9051cd28d7
epwo <ewann.postic@isen-ouest.yncrea.fr>
2025-06-11 18:30:17 +0200

    Add production configuration for basePath and assetPrefix in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 };
+// next.config.js
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  basePath: "/blog",
+  assetPrefix: isProd ? "/blog/" : "",
+};
 
 export default nextConfig;
