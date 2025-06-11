4deaa47d3238045dd60d745642196011f57089e5
epwo <ewann.postic@isen-ouest.yncrea.fr>
2025-06-11 20:46:20 +0200

    Update next.config.ts to remove unused configurations and set basePath for blog deployment; adjust homepage in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/blog",
   output: "export",
-  basePath: "", // no basePath because you deploy to root of subdomain
-  assetPrefix: "", // no asset prefix
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  reactStrictMode: true,
 };
-
 module.exports = nextConfig;
