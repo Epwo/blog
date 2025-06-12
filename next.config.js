0817bc3fb81931041ebb0fdf741cb0608dca71d1
epwo <ewann.postic@isen-ouest.yncrea.fr>
2025-06-12 19:47:02 +0200

    Remove unused configuration options from next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,4 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   basePath: isProd ? "/blog" : "",
   assetPrefix: isProd ? "/blog/" : "",
-  reactStrictMode: true,
-  // Use trailingSlash: true if you want URLs to end with a /
-  trailingSlash: false,
 };
